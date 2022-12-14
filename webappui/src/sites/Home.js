4c1c53a906c63c03e492a4e967fe07c56acfa8c6
t33k00 <tmkyllonen@gmail.com>
2022-12-14 17:24:31 +0200

    v1.1final

--- webappui/src/sites/Home.js
+++ webappui/src/sites/Home.js
@@ -9,14 +9,15 @@ export default function Home(props) {
 
   return (
     <div>
-      <div class="container">
+      <div class="linkbtn">
         {props.userLoggedIn ?
-          <div class="btn"><a href="protected">Go to charts</a></div>
+          <Link to="protected">Go to charts</Link>
           :
           <>
-            <div class="btn"><a href="register">Sign up</a></div>
-            <div class="btn"><a href="login">login</a></div>
+            <Link to="register">Sign up</Link><br />
+            <Link to="login">Login</Link>
           </>
+
 
         }
 
